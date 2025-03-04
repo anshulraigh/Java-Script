3c0afcdb195776bdb6b403c84af979c486c1bdd4
Anshul Rai <anshulrai02092003@gmail.com>
2025-03-04 16:46:44 +0530

    [Anshul] Add. map

--- functions/PallindromicNumbers.js
+++
@@ -1,6 +0,0 @@
-function isPalindrome(num) {
-    let reversed = parseInt(num.toString().split('').reverse().join(''));
-    return num === reversed;
-}
-console.log(isPalindrome(121)); 
-console.log(isPalindrome(123));
