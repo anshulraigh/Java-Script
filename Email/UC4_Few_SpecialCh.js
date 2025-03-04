f5fd96dc7298d21216913794a0e8455621eb299c
Anshul Rai <anshulrai02092003@gmail.com>
2025-03-04 09:50:08 +0530

    [Anshul] Add. UC4 - making sure only _ , +, -, . are valid

---
+++ Email/UC4_Few_SpecialCh.js
@@ -0,0 +1,12 @@
+function validateEmail(email) {
+    const emailPattern = /^abc([_+-.]xyz)?@bridgelabz\.co(\.[a-zA-Z]{2,})?$/;
+    return emailPattern.test(email);
+}
+
+// Test cases
+console.log(validateEmail("abc@bridgelabz.co"));       
+console.log(validateEmail("abc@bridgelabz.co.in"));    
+console.log(validateEmail("abc.xyz@bridgelabz.co.in"));
+console.log(validateEmail("abc@xyz.co.in"));           
+console.log(validateEmail("abc@bridgelabzcom"));    
+console.log(validateEmail("abc$xyz@bridgelabz.co"));   
