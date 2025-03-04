09fac1c0aef2eacaa73e3ca3e6b1c85d180e048a
Anshul Rai <anshulrai02092003@gmail.com>
2025-03-04 09:47:44 +0530

    [Anshul] Add. UC3 - Restrict taking alphabet and special characters at the end

---
+++ Pincode/UC3_Alpha_SPecialCH.js
@@ -0,0 +1,13 @@
+function validatePinCode(pin) {
+    const pinPattern = /^[0-9]{6}$/;
+    return pinPattern.test(pin);
+}
+
+// Test cases
+console.log(validatePinCode("400088"));  
+console.log(validatePinCode("A400088")); 
+console.log(validatePinCode("400088B")); 
+console.log(validatePinCode("40008A"));  
+console.log(validatePinCode("400088 "));
+console.log(validatePinCode("1234@6"));  
+console.log(validatePinCode("4000887")); 
